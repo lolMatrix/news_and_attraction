5769dca0c697ff8930b1997b9a69ad2849a6e12c
MatriX <dr.lesha@live.com>
2022-01-08 16:47:17 +0300

    lib update

--- weblib/mongo_api.py
+++ weblib/mongo_api.py
@@ -10,8 +10,7 @@ def get_news_list():
     return requests.get(weblib.url + "/news/").json()
 
 def save_news(data: dict):
-    jsn = json.dumps(data)
-    return requests.post(weblib.url + "/news/save/", json=jsn).json()
+    return requests.post(weblib.url + "/news/save/", json=data).json()
 
 def update_news(update: dict):
     return requests.put(weblib.url + "/news/update/", json=update)
