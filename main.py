1d747afbd7829a066f0ba5ac6c29ff403b723283
TarakanVS <tor6515@gmail.com>
2022-01-10 19:08:07 +0300

    Задание 2, добавил томиту, пытаюсь настроить работу скрипта внутри проекта.

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 from web import manage
 from threading import Thread
 from caruler.kurs import spider
+from tomitaworker import TomitaWorker
 
 if __name__ == "__main__":
     thread = Thread(target=spider.start_spider, daemon=True)
     thread.start()
+    threadTomita = Thread(target=TomitaWorker.run(), daemon=True)
+    threadTomita.start()
     manage.runserver()
