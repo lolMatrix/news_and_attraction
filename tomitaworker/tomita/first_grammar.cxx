53d62c5b53e21c7b60e9b02581f6badff3e36150
TarakanVS <tor6515@gmail.com>
2022-01-11 16:01:25 +0300

    Добавлены файлы самого томита-парсера

---
+++ tomitaworker/tomita/first_grammar.cxx
@@ -0,0 +1,7 @@
+﻿#encoding "utf-8"
+
+Person -> AnyWord<kwtype=politicians>;
+Object -> AnyWord<kwtype=objects>;
+
+Fact -> Person interp (Fact.Politician);
+Fact -> Object interp (Fact.Attraction);
