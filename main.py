99b04f94379330dc73ba99838bcc40febe4dab14
TarakanVS <tor6515@gmail.com>
2022-01-11 14:50:12 +0300

    Добавлен бесконечный цикл в tomita_worker, перенёс всю работу на mongo_api

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 from web import manage
 from threading import Thread
 from caruler.kurs import spider
-from tomitaworker import TomitaWorker
+from tomitaworker import tomita_worker
 
 if __name__ == "__main__":
     thread = Thread(target=spider.start_spider, daemon=True)
     thread.start()
-    threadTomita = Thread(target=TomitaWorker.run(), daemon=True)
+    threadTomita = Thread(target=tomita_worker.start_tomita(), daemon=True)
     threadTomita.start()
     manage.runserver()
---
+++ tomitaworker/tomita_worker.py
@@ -0,0 +1,39 @@
+from weblib import mongo_api
+import os
+
+
+def run():
+    collection = mongo_api.get_news_list()
+    for news in collection["news"]:
+        with open("../tomita/input.txt", "w") as file:
+            file.write(news['text'])
+        os.system("../tomita/tomita-parser cfg.proto")
+
+        politician = []
+        attraction = []
+        place = ""
+        with open("../tomita/output.txt", "r") as file:
+            file_output = file.read()
+            words = file_output.split()
+            for i in range(len(words)):
+                if words[i] == "Politician":
+                    if words[i + 2] not in politician:
+                        politician.append(words[i + 2])
+                    elif words[i] == "Attraction":
+                        if words[i + 2] not in place:
+                            attraction.append(words[i + 2])
+
+        for i in politician:
+            collection["tomita"] = ({"Person": i, "News": news['text']})
+
+        for j in attraction:
+            collection["tomita"] = ({"Object": j, "News": news['text']})
+
+            mongo_api.save_news(collection)
+            politician.clear()
+            attraction.clear()
+
+
+def start_tomita():
+    while True:
+        run()
