d52925330954a605037c6e2708de0e217d87277d
MatriX <dr.lesha@live.com>
2022-01-20 23:25:28 +0300

    fixes

--- rating/rating_worker.py
+++ rating/rating_worker.py
@@ -48,13 +48,7 @@ def get_tweets_for_model(cleaned_tokens_list):
     for tweet_tokens in cleaned_tokens_list:
         yield dict([token, True] for token in tweet_tokens)
 
-
-def run():
-    positive_tweets = twitter_samples.strings('positive_tweets.json')
-    negative_tweets = twitter_samples.strings('negative_tweets.json')
-    text = twitter_samples.strings('tweets.20150430-223406.json')
-    tweet_tokens = twitter_samples.tokenized('positive_tweets.json')[0]
-
+def init():
     stop_words = stopwords.words('english')
 
     positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
@@ -68,11 +62,6 @@ def run():
 
     for tokens in negative_tweet_tokens:
         negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
-
-    all_pos_words = get_all_words(positive_cleaned_tokens_list)
-
-    freq_dist_pos = FreqDist(all_pos_words)
-    print(freq_dist_pos.most_common(10))
 
     positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
     negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
@@ -88,27 +77,23 @@ def run():
     random.shuffle(dataset)
 
     train_data = dataset[:7000]
-    test_data = dataset[7000:]
 
     classifier = NaiveBayesClassifier.train(train_data)
 
-    print("Accuracy is:", classify.accuracy(classifier, test_data))
 
-    print(classifier.show_most_informative_features(10))
-
+def run():
     # Оценка предложений из БД
     collection = mongo_api.get_news_list()
 
     for news in collection:
         if "tomita" in news:
-            news["rate"] = []
             tweet = news["text"]
-            custom_tokens = []
             custom_tokens = remove_noise(word_tokenize(tweet))
-            news["rate"].append({"Rating": classifier.classify(dict([token, True] for token in custom_tokens))})
+            news["rate"] = classifier.classify(dict([token, True] for token in custom_tokens))
             mongo_api.update_news(news)
 
 def start_rating():
+    init()
     while True:
         try:
             run()
