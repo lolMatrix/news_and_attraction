75928f3a913a405d8ea49fecc1b7fd523c5faf1b
TarakanVS <tor6515@gmail.com>
2022-01-20 13:10:55 +0300

    feature/word_2_vec обавлены файлы для поиска синонимов

---
+++ word2vecworker/word_2_vec_worker_user.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import Word2VecModel
+
+MODEL_PATH = "model"
+
+spark = SparkSession \
+    .builder \
+    .appName("word2vec") \
+    .getOrCreate()
+
+model = Word2VecModel.load(MODEL_PATH)
+count = 10
+
+while(True):
+    try:
+        word = input("[INPUT] Введите слово из словаря: ")
+        if(word == "--exit"):
+            break
+        elif(word == "--count"):
+            count = int(input("[INPUT] Введите кол-во синонимов: "))
+            print(count)
+        else:
+            model.findSynonyms(word, count).show(n=count)
+    except Exception as ex:
+        print("[ERROR] Данного слова нет в словаре!")
+
+spark.stop()
