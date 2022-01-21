4016e8db6699b5eab86f02640ada5ffebad48c13
TarakanVS <tor6515@gmail.com>
2022-01-21 22:10:52 +0300

    fixes фиксы томиты

--- tomitaworker/tomita_worker.py
+++ tomitaworker/tomita_worker.py
@@ -18,6 +18,8 @@ def run():
             os.system("./tomitaworker/tomita/tomita-parser ./tomitaworker/tomita/config.proto")
             politician = []
             attraction = []
+            attr_sent = []
+            pol_sent = []
 
             with open("tomitaworker/tomita/output.txt", "r") as file:
                 file_output = file.read()
@@ -27,19 +29,33 @@ def run():
                         words = strings[i].split("Politician =")
                         if words[1] not in politician:
                             politician.append(words[1].strip().replace("_", " "))
+                            counter = 0
+                            while "{" in strings[i - counter] or "}" in strings[i - counter] \
+                                    or "Fact" in strings[i - counter] or "Politician" in strings[i - counter]:
+                                counter += 1
+
+                            pol_sent.append(strings[i - counter])
+
                     elif "Attraction" in strings[i]:
                         words = strings[i].split("Attraction =")
                         if words[1] not in attraction:
                             attraction.append(words[1].strip().replace("_", " "))
 
+                            counter = 0
+                            while "{" in strings[i - counter] or "}" in strings[i - counter] \
+                                    or "Fact" in strings[i - counter] or "Attraction" in strings[i - counter]:
+                                counter += 1
+
+                            attr_sent.append(strings[i - counter])
+
             if len(politician) > 0 or len(attraction) > 0:
                 news["tomita"] = []
 
-            for i in politician:
-                news["tomita"].append({"Person": i})
+            for i in range(len(politician)):
+                news["tomita"].append({"Person": politician[i], "Sentence": pol_sent[i]})
 
-            for j in attraction:
-                news["tomita"].append({"Attraction": j})
+            for j in range(len(attraction)):
+                news["tomita"].append({"Attraction": attraction[j], "Sentence": attr_sent[j]})
 
             mongo_api.update_news(news)
 
